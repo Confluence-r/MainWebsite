b9a8f7d38327e8831efc2227615f41eb24a6007f
Akshay Bhopani <akshaybhopani@gmail.com>
2021-09-15 15:07:42 +0530

    New Mobile Navbar

--- pages/home.js
+++ pages/home.js
@@ -6,20 +6,48 @@ import ThemeSwitch from './ThemeSwtch.js';
 import Head from "next/head"
  
 export default function Home() {
+  useEffect(() => {
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+  navBar.classList.toggle('toggle');
+});
+});
+
 const [theme, settheme] = useState("");
   return (
   	<div className={styles.home}>
      <Head>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet"/>
   </Head>
+   <div class="container">
+<nav class="header__nav nav-bar">
+    <div class="toggle-menu">
+      <div class="line line1"></div>
+      <div class="line line2"></div>
+      <div class="line line3"></div>
+    </div>
+    <ul class="nav-list">
+      <li class="nav-list-item"><a href="" class="nav-link">Home</a></li>
+      <li class="nav-list-item"><a href="./#about" class="nav-link">About</a></li>
+      <li class="nav-list-item">
+        <a href="" class="nav-link" href="./#blog">Blog</a>
+      </li>
+      <li class="nav-list-item">
+ <a href="./#contact-us" class="nav-link">Contact Us</a>
+      </li>
+    </ul>
+  </nav>
+</div>
+ 
 	<div className={`${styles.box} ${styles.yellow} ${styles.main}`}>
 		<Image layout="fill" src={`/light/uparrow${theme}.gif`}/><div className={styles.middle}>
-    <div className={styles.text}>HOME</div>
+    <div className={styles.text} id="home">HOME</div>
   </div></div>
 	<a href="./" className={`${styles.box} ${styles.white}`}>
 	<Image layout="fill" src={`/light/river${theme}.gif`}/>
 		<div className={styles.middle}>
-    <div className={styles.text}>ABOUT US</div>
+    <div className={styles.text} id="about">ABOUT US</div>
   </div>
 	</a>
 	<a href="./" className={`${styles.box2} ${styles.orange}`}>
@@ -35,7 +63,7 @@ const [theme, settheme] = useState("");
 	<a  href="./" className={`${styles.box} ${styles.lightblue}`}>
 <Image layout="fill" src={`/light/Arrows${theme}.gif`}/>
 	<div className={styles.middle}>
-    <div className={styles.text}>BLOG</div>
+    <div className={styles.text} id="blog">BLOG</div>
   </div></a>
 	<a href="./" className={`${styles.box} ${styles.yellow}`}>
 <Image layout="fill" src={`/light/confluencer${theme}.gif`}/>
@@ -55,10 +83,10 @@ const [theme, settheme] = useState("");
   <a href="./" className={`${styles.box2} ${styles.white}`}>
   <Image layout="fill" src={`/light/logo${theme}.png`}/>
   <div className={styles.middle}>
-    <div className={styles.text}>CONTACT US</div>
+    <div className={styles.text} id="contact-us">CONTACT US</div>
   </div></a>
   <ThemeSwitch theme={theme} settheme={settheme}/>
-  
+
 </div>
 
   	)
