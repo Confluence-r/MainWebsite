ec84b0cbc307fc1e739a11631f80725ee60cc663
Akshay Bhopani <akshaybhopani@gmail.com>
2021-09-20 15:41:12 +0530

    Changelog 20Sep

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,22 @@ import ThemeSwitch from './ThemeSwtch.js';
 import Head from "next/head"
 import Link from "next/link"
 
-export default function Home() {
+export default function Home(theme) {
   useEffect(() => {
 var toggleButton = document.querySelector('.toggle-menu');
-var navBar = document.querySelector('.nav-bar');
-toggleButton.addEventListener('click', function () {
-  navBar.classList.toggle('toggle');
-});
+var navbar = document.querySelector('.header');
+// toggleButton.addEventListener('click', function () {
+//   navbar.classList.toggle('toggle');
+// });
 });
 
-const [theme, settheme] = useState("");
   return (
   	<div className={styles.home}>
      <Head>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet"/>
   </Head>
    <div class="container">
-<nav class="header__nav nav-bar">
+{/*<nav class="header__nav nav-bar">
     <div class="toggle-menu">
       <div class="line line1"></div>
       <div class="line line2"></div>
@@ -38,21 +37,21 @@ const [theme, settheme] = useState("");
  <a href="./#contact-us" class="nav-link">Contact Us</a>
       </li>
     </ul>
-  </nav>
+  </nav>*/}
 </div>
- 
+
 	<a href="./" className={`${styles.box} ${styles.yellow} ${styles.main}`}>
-		<Image layout="fill" src={`/light/uparrow${theme}.gif`}/><div className={styles.middle}>
+		<Image layout="fill" src={`/light/uparrow${theme.theme}.gif`}/><div className={styles.middle}>
     <div className={styles.text} id="home">HOME</div>
   </div></a>
 	<Link href="./about"><a className={`${styles.box} ${styles.white}`}>
-	<Image layout="fill" src={`/light/river${theme}.gif`}/>
+	<Image layout="fill" src={`/light/river${theme.theme}.gif`}/>
 		<div className={styles.middle}>
     <div className={styles.text} id="about">ABOUT US</div>
   </div>
 	</a></Link>
 	<Link href="./our-work"><a href="./" className={`${styles.box2} ${styles.orange}`}>
-	<Image layout="fill" src={`/light/button${theme}.gif`}/>
+	<Image layout="fill" src={`/light/button${theme.theme}.gif`}/>
 	<div className={styles.middle}>
     <div className={styles.text}>OUR WORK</div>
   </div></a></Link>
@@ -62,31 +61,31 @@ const [theme, settheme] = useState("");
     <div className={styles.text}></div>
   </div></a>
 	<a  href="https://blog.confluence-r.com" className={`${styles.box} ${styles.lightblue}`}>
-<Image layout="fill" src={`/light/Arrows${theme}.gif`}/>
+<Image layout="fill" src={`/light/Arrows${theme.theme}.gif`}/>
 	<div className={styles.middle}>
     <div className={styles.text} id="blog">BLOG</div>
   </div></a>
 	<a  className={`${styles.box} ${styles.yellow}`}>
-<Image layout="fill" src={`/light/confluencer${theme}.gif`}/>
+<Image layout="fill" src={`/light/confluencer${theme.theme}.gif`}/>
 	<div className={styles.middle}>
     <div className={styles.text}></div>
   </div></a>
 	<a className={`${styles.box} ${styles.white}`}>
-<Image layout="fill" src={`/light/lines${theme}.png`}/>
+<Image layout="fill" src={`/light/lines${theme.theme}.png`}/>
   <div className={styles.middle}>
     <div className={styles.text}></div>
   </div></a>
 	<a href="https://www.instagram.com/confluencer_/?hl=en" className={`${styles.box} ${styles.orange}`}>
-<Image layout="fill" src={`/light/rings${theme}.gif`}/>
+<Image layout="fill" src={`/light/rings${theme.theme}.gif`}/>
 	<div className={styles.middle}>
     <div className={styles.text}></div>
   </div></a>
   <Link href="./contact"><a href="./" className={`${styles.box2} ${styles.white}`}>
-  <Image layout="fill" src={`/light/logo${theme}.png`}/>
+  <Image layout="fill" src={`/light/logo${theme.theme}.png`}/>
   <div className={styles.middle}>
     <div className={styles.text} id="contact-us">CONTACT US</div>
   </div></a></Link>
-  <ThemeSwitch theme={theme} settheme={settheme}/>
+ 
 <a class="arrow" href="#programmes"><i class="fas fa-angle-down"></i></a>
 </div>
 
--- pages/our-work.js
+++ pages/our-work.js
@@ -4,17 +4,7 @@ export default function OurWork() {
 	return (
 	<>
 
-<header class="header">
-  <a href="./" class="logo"><Image src="/logo.png" width="150px" height="50%"/></a>
-  <input class="menu-btn" type="checkbox" id="menu-btn" />
-  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
-  <ul class="menu">
-    <li><a href="#work">Our Work</a></li>
-    <li><a href="#about">About</a></li>
-    <li><a href="#careers">Careers</a></li>
-    <li><a href="#contact">Contact</a></li>
-  </ul>
-</header>
+
 
 <br/><br/><br/><br/>
 <div class="Container">
@@ -25,7 +15,7 @@ export default function OurWork() {
 <div class="button-wrapper">
 <br/>
 <i class="fas fa-quote-left"></i>
-       The talented team at Confluence- r, understood the brief given at the first instant and have created a top class web site by sharing innovative ideas and visuals and hand holding us through the whole process. A very professional and talented organization – Viral Shah, Partner
+       <p>The talented team at Confluence- r, understood the brief given at the first instant and have created a top class web site by sharing innovative ideas and visuals and hand holding us through the whole process. A very professional and talented organization – Viral Shah, Partner</p>
        <i class="fas fa-quote-right"></i>
        <br/><br/><br/><br/>
 <a class="button">Review</a><br/><br/><br/><br/>
@@ -35,9 +25,6 @@ export default function OurWork() {
 </div>
 <br/><br/><br/><br/>
 
-<footer>
-  <p>All rights reserved. Confluence-r 2021</p>
-</footer>
 	</>
 
 )}
